3badd5a46da9173060f6dc71b487502923cf4d37
htLiang <l15304704058@126.com>
2026-04-03 19:36:56 +0800

    feat: Add the example about the life cycle of servlet

---
+++ 05-Servlet-Demo/src/main/java/com/study/servlet/ServletLifeCycle.java
@@ -0,0 +1,42 @@
+package com.study.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Arrays;
+
+/**
+ * ClassName: Practice01
+ * Package: java001.day05
+ * Description:
+ * {@code @Author} Liang-ht
+ * {@code @Create} 2025/9/27 14:19
+ * {@code @Version} 1.0
+ */
+@WebServlet(value = "/slc",loadOnStartup = 100)
+public class ServletLifeCycle extends HttpServlet {
+
+	public ServletLifeCycle() {
+		System.out.println("构造器执行");
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("初始化");
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("服务");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("销毁");
+	}
+}
