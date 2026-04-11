c00ea054318db2852a4273d03c07bd43fc94b2fa
htLiang <l15304704058@126.com>
2026-04-11 13:09:30 +0800

    ✨ feat: 00-ts前提知识点-泛型接口自定义类型

---
+++ 11-Frontend-Engineering-Demo/demo03-vue-basic/study-Demo/00-ts前提知识点-泛型接口自定义类型/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <Person/>
+</template>
+
+<script lang="ts" name="App" setup>
+import Person from './components/Person.vue'
+</script>
+
+<style></style>
---
+++ 11-Frontend-Engineering-Demo/demo03-vue-basic/study-Demo/00-ts前提知识点-泛型接口自定义类型/components/Person.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="person">
+
+    </div>
+</template>
+
+
+<script lang="ts" setup>
+import {type Persons} from "@/types"
+
+// let person: PersonInter = {id: 'daskfjk01', name: '张三', age: 40};
+
+// 使用泛型
+// let personList:Array<PersonInter> =[
+//     {id: 'daskfjk01', name: '张三', age: 40},
+//     {id: 'daskfjk02', name: '李四', age: 55},
+//     {id: 'daskfjk03', name: '王五', age: 33}
+// ]
+// 在main.ts中声明一个自定义对象
+let personList: Persons = [
+    {id: 'daskfjk01', name: '张三', age: 40},
+    {id: 'daskfjk02', name: '李四', age: 55},
+    {id: 'daskfjk03', name: '王五', age: 33}
+]
+</script>
+
+<style>
+
+</style>
