ec230bf6dc465be8ee5976aa987bcb9cb04256a0
htLiang <l15304704058@126.com>
2026-04-11 13:09:31 +0800

    ✨ feat: 01-fist-project

---
+++ 11-Frontend-Engineering-Demo/demo03-vue-basic/study-Demo/01-fist-project/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <h1>你好啊！</h1>
+  </div>
+  <Person/>
+</template>
+
+<script lang="ts">
+import Person from "@/components/Person.vue";
+
+export default {
+  name:'App',
+  components: {Person}
+  //组件名
+}
+</script>
+
+<style>
+  .app{
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+
+</style>
