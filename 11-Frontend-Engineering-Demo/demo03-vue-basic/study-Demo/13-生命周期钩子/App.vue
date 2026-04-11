40ba883244a7ec336cac2132084463d74f5d9334
htLiang <l15304704058@126.com>
2026-04-11 13:09:31 +0800

    ✨ feat: 13-生命周期钩子

---
+++ 11-Frontend-Engineering-Demo/demo03-vue-basic/study-Demo/13-生命周期钩子/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <Person v-if="isShow"/>
+</template>
+
+<script lang="ts" name="App" setup>
+import Person from './components/Person.vue'
+import {ref} from "vue";
+
+let isShow = ref(true);
+
+</script>
+
+<style></style>
---
+++ 11-Frontend-Engineering-Demo/demo03-vue-basic/study-Demo/13-生命周期钩子/components/Person.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="person">
+        <h2>当前求和为{{sum}}</h2>
+        <button @click="sumAdd">点击求和+1</button>
+    </div>
+</template>
+
+
+<script lang="ts" setup>
+import {onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref} from "vue";
+
+let sum = ref(1);
+
+function sumAdd(){
+    sum.value++;
+}
+// 创建   setup替代了beforeCreate/created
+console.log('创建');
+
+//挂载,在挂载前调用onBeforeMount中传入的回掉函数
+onBeforeMount(()=>{
+    console.log('挂载前')
+})
+onMounted(()=>{
+    console.log('挂载完毕')
+})
+// 更新
+onBeforeUpdate(()=>{
+    console.log('更新前')
+})
+onUpdated(()=>{
+    console.log('更新完毕')
+})
+//卸载
+onBeforeUnmount(()=>{
+    console.log('卸载前')
+})
+onUnmounted(()=>{
+    console.log('卸载完毕')
+})
+
+</script>
+
+<style scoped>
+    .person{
+        background-color: skyblue;
+        padding: 10px;
+        border-radius: 10px;
+        box-shadow: 0 0 10px;
+    }
+</style>
