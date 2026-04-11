01c3855d5471d3309b7e88ae8ff5179a97854faf
htLiang <l15304704058@126.com>
2026-04-11 13:09:31 +0800

    ✨ feat: 22-属性渲染

---
+++ 11-Frontend-Engineering-Demo/demo03-vue-basic/study-Demo/22-属性渲染/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts" name="App" setup>
+const data = {
+    logo: "https://cdn.jsdelivr.net/gh/Shadow1086/myPicture@master/uPic/2026/04/06/16-56-95Nk0B",
+    name:"尚硅谷",
+    url : "https://www.baidu.com"
+}
+</script>
+
+<template>
+    <div>
+        <img v-bind:src="data.logo" v-bind:title="data.name">
+        <a v-bind:href="data.url"></a>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
