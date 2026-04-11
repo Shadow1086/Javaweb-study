d6882ff0e53e73b9cd661ab3efc08280ab310e50
htLiang <l15304704058@126.com>
2026-04-11 13:09:31 +0800

    ✨ feat: 02-setup概述

---
+++ 11-Frontend-Engineering-Demo/demo03-vue-basic/study-Demo/02-setup概述/components/Person.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Person',
+  beforeCreate(): any { //声明周期
+
+  },
+  setup() {
+    // 数据 ----- 注意：此时的数据不是响应式的
+    let name = "zhangsan"
+    let age = 18
+    let tel = '13888888888'
+
+    //方法,setup函数中的this是undefined
+    function changeName() {
+      name = '张三'     //这样修改，页面没有变化，但确实修改了
+    }
+
+    function changeAge() {
+      age++
+    }
+
+    function showTel() {
+      alert(tel)
+    }
+
+    // 将数据方法交出去，模块中才可以使用
+    return {name, age, tel,changeName,changeAge,showTel}
+
+    // setup返回值也可以是一个渲染函数
+    // return ()=> '哈哈'
+
+  }
+}
+</script>
+
+<style>
+.person {
+  background-color: skyblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
