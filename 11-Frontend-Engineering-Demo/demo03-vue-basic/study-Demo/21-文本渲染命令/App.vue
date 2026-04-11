1dc79a71b2e937acadbf1d8638cd5dc782fcfa41
htLiang <l15304704058@126.com>
2026-04-11 13:09:31 +0800

    ✨ feat: 21-文本渲染命令

---
+++ 11-Frontend-Engineering-Demo/demo03-vue-basic/study-Demo/21-文本渲染命令/App.vue
@@ -0,0 +1,32 @@
+<script lang="ts" name="App" setup>
+/*
+* 文本渲染
+*   v-text
+*   v-html
+* */
+let msg = "hello vue3"
+let haha = "haha";
+let msg2 = `hello ${haha}`
+
+let age = 19;
+let bee = "蜜 蜂"
+function print(){
+    return "函数调用";
+}
+</script>
+
+<template>
+    <div>
+        <h1 v-text="msg"></h1>
+        <h1 v-text="msg2"></h1>
+        <h1 v-text="`你好 ${haha}`"></h1>
+        <h1 v-text="age>=18?'是':'否'"></h1>
+        <h1 v-text="bee.split(' ').reverse().join('')"></h1>
+        <h1 v-text="print()"></h1>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
