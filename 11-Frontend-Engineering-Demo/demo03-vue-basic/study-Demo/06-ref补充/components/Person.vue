6b88f7e45020c3a11773202baca781cdb7f32dd3
htLiang <l15304704058@126.com>
2026-04-11 13:09:31 +0800

    ✨ feat: 06-ref补充

---
+++ 11-Frontend-Engineering-Demo/demo03-vue-basic/study-Demo/06-ref补充/components/Person.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="person">
+		<h2>一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
+		<button @click="changePrice">修改汽车价格</button>
+		<br>
+		<h2>游戏列表：</h2>
+		<ul>
+			<li v-for="game in games" :key="game.id">{{ game.name }}</li>
+		</ul>
+		<button @click="changeFirstGame">修改第一个游戏名称</button>
+		<!--        <hr>-->
+		<!--        <div>测试数据：{{ obj.a.b.c }}</div>-->
+		<!--        <button @click="changeTestNum">修改测试数据</button>-->
+	</div>
+</template>
+
+<script lang="ts" name="Person" setup>
+import {ref} from 'vue'
+//数据
+let car = ref({brand: '奔驰', price: 100});
+let games = ref([
+	{id: 'asdfjklasdf01', name: '王者'},
+	{id: 'asdfjklasdf02', name: '和平'},
+	{id: 'asdfjklasdf03', name: '吃鸡'}
+])
+
+//方法
+function changePrice() {
+	car.value.price += 10;
+}
+
+function changeFirstGame() {
+	games.value[0].name = '原神';
+}
+
+//
+// function changeTestNum() {
+//     obj.a.b.c = 888;
+// }
+</script>
+
+<style scoped>
+.person {
+	background-color: skyblue;
+	box-shadow: 0 0 10px;
+	border-radius: 10px;
+	padding: 20px;
+}
+
+li {
+	font-size: 20px;
+}
+
+</style>
