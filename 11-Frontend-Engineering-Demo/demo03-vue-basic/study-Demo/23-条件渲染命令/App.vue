4c43e138227e4780ecd8d0eec1adf2d00d6878f4
htLiang <l15304704058@126.com>
2026-04-11 13:09:31 +0800

    ✨ feat: 23-条件渲染命令

---
+++ 11-Frontend-Engineering-Demo/demo03-vue-basic/study-Demo/23-条件渲染命令/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts" name="App" setup>
+import {ref} from "vue";
+
+let flag = ref(true);
+let reverse = (flag: boolean) => {
+    flag = !flag;
+}
+
+</script>
+
+<template>
+    <div>
+        <h1 v-if="flag">vue is awesome!</h1>
+        <h1 v-else>oh on!</h1>
+        <button @click="reverse(flag)">更改</button>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
